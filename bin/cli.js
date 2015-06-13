9eb47d8546fe6750f9a0d305a48f724f31be997d
David da Silva Contin <dasilvacontin@gmail.com>
2015-06-13 20:32:10 +0200

    cli is functional

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,8 @@
 #! /usr/bin/env node
 
+var fs = require('fs')
 var program = require('commander')
+
 var pkg = require('../package.json')
 var sauce = require('../lib/gh-sauce.js')
 
@@ -11,13 +13,35 @@ program
 
 var files = program.args
 if (files.length === 0) {
-	files = ['README.md', 'CHANGELOG.md']
+  files = ['README.md', 'CHANGELOG.md']
 }
 
-console.log('Dressing ' + files.join(', ') + ' with some gh-sauce...')
+console.log('# Dressing ' + files.join(', ') + ' with some gh-sauce...\n')
+
+function doneDressing (msg) {
+  console.log(msg)
+  --files.length
+  if (files.length === 0) {
+    console.log('\nDone! 🍧')
+  }
+}
 
 files.forEach(function (file) {
-	sauce.dress(file)
+  fs.readFile(file, function (err, data) {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        doneDressing('- [ ] "' + file + '" doesn\'t exist')
+      } else {
+        doneDressing('- [ ] "' + file + '" error, code :' + err.code)
+      }
+      return
+    }
+    var dressed = sauce.dress(data.toString())
+    fs.writeFile(file, dressed, function (err) {
+      if (err) {
+        throw err
+      }
+      doneDressing('- [x] "' + file + '" was dressed with gh-sauce')
+    })
+  })
 })
-
-console.log('Done! 🍧')
