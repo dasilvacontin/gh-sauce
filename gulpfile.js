0096fb5778a1e7b58a9bcdadc9c4fce856d3dca5
David da Silva Contin <dasilvacontin@gmail.com>
2015-06-13 13:08:39 +0200

    lint tests too

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp   = require('gulp');
 var plugins = require('gulp-load-plugins')();
 
 var paths = {
-  lint: ['./gulpfile.js', './lib/**/*.js'],
+  lint: ['./gulpfile.js', './lib/**/*.js', './test/**/*.js'],
   watch: ['./gulpfile.js', './lib/**', './test/**/*.js', '!test/{temp,temp/**}'],
   tests: ['./test/**/*.js', '!test/{temp,temp/**}'],
   source: ['./lib/*.js']
